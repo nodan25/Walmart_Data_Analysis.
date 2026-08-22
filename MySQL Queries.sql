f7ae62208190e16de480559c42a9557d29430525
nodanbhatia <nodanbhatia376@gmail.com>
2026-08-22 13:23:12 +0530

    Update MySQL Queries.sql

--- MySQL Queries.sql
+++ MySQL Queries.sql
@@ -1,28 +1,22 @@
--- Walmart Project Queries - MySQL
+
 
 SELECT * FROM walmart;
 
--- DROP TABLE walmart;
-
--- DROP TABLE walmart;
-
--- Count total records
 SELECT COUNT(*) FROM walmart;
 
--- Count payment methods and number of transactions by payment method
+
 SELECT 
     payment_method,
     COUNT(*) AS no_payments
 FROM walmart
 GROUP BY payment_method;
 
--- Count distinct branches
+
 SELECT COUNT(DISTINCT branch) FROM walmart;
 
--- Find the minimum quantity sold
+
 SELECT MIN(quantity) FROM walmart;
 
--- Business Problem Q1: Find different payment methods, number of transactions, and quantity sold by payment method
 SELECT 
     payment_method,
     COUNT(*) AS no_payments,
@@ -30,8 +24,6 @@ SELECT
 FROM walmart
 GROUP BY payment_method;
 
--- Project Question #2: Identify the highest-rated category in each branch
--- Display the branch, category, and avg rating
 SELECT branch, category, avg_rating
 FROM (
     SELECT 
@@ -44,7 +36,6 @@ FROM (
 ) AS ranked
 WHERE rank = 1;
 
--- Q3: Identify the busiest day for each branch based on the number of transactions
 SELECT branch, day_name, no_transactions
 FROM (
     SELECT 
@@ -57,14 +48,12 @@ FROM (
 ) AS ranked
 WHERE rank = 1;
 
--- Q4: Calculate the total quantity of items sold per payment method
 SELECT 
     payment_method,
     SUM(quantity) AS no_qty_sold
 FROM walmart
 GROUP BY payment_method;
 
--- Q5: Determine the average, minimum, and maximum rating of categories for each city
 SELECT 
     city,
     category,
@@ -74,7 +63,6 @@ SELECT
 FROM walmart
 GROUP BY city, category;
 
--- Q6: Calculate the total profit for each category
 SELECT 
     category,
     SUM(unit_price * quantity * profit_margin) AS total_profit
@@ -82,7 +70,6 @@ FROM walmart
 GROUP BY category
 ORDER BY total_profit DESC;
 
--- Q7: Determine the most common payment method for each branch
 WITH cte AS (
     SELECT 
         branch,
@@ -96,7 +83,6 @@ SELECT branch, payment_method AS preferred_payment_method
 FROM cte
 WHERE rank = 1;
 
--- Q8: Categorize sales into Morning, Afternoon, and Evening shifts
 SELECT
     branch,
     CASE 
@@ -109,7 +95,6 @@ FROM walmart
 GROUP BY branch, shift
 ORDER BY branch, num_invoices DESC;
 
--- Q9: Identify the 5 branches with the highest revenue decrease ratio from last year to current year (e.g., 2022 to 2023)
 WITH revenue_2022 AS (
     SELECT 
         branch,
